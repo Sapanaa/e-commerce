c57ce6c7c5fdfd9a72ffd66aa0d256f574bef0fc
sapana <sapanadhami1111@gmail.com>
2026-01-22 14:29:32 +0000

    npm update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -41,9 +41,7 @@ const products = [
 ];
 
 const Home = async () => {
-  const user = await getCurrentUser();
 
-  console.log('USER:', user);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
